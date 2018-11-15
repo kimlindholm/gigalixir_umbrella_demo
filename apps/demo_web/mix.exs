eada3db1408195aadbd24edf7d9f208f5a8d187a
Kim Lindholm <misc@dynevision.com>
2018-11-15 22:16:16 +0700

    Update Elixir version

--- apps/demo_web/mix.exs
+++ apps/demo_web/mix.exs
@@ -9,7 +9,7 @@ defmodule DemoWeb.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
